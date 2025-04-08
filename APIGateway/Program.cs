9a2717436296881cb59fcca332d05f304bdf484f
Mohit Verma <mohitverma.540.mv@gmail.com>
2025-04-08 21:58:02 +0530

    api gateway fixed

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,6 +1,21 @@
+using Yarp.ReverseProxy;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapReverseProxy();
+});
 app.Run();
